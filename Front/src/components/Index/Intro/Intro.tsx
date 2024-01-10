d7e47f0eda28936eb8e0d14955db02adc30f83b0
Jaswine <vaselesstolarov813@gmail.com>
2024-01-10 23:14:59 +0600

    updated constructor

--- Front/src/components/Index/Intro/Intro.tsx
+++ Front/src/components/Index/Intro/Intro.tsx
@@ -32,7 +32,7 @@ const Intro:FC = () => {
             <div className={styles.intro__right}>
                 <span className={styles.intro__right__violet}></span>
                 <span className={styles.intro__right__pink}></span>
-                <img src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="intro__image" />
+                <img src="/public/constructor.png" alt="intro__image" />
             </div>
         </div>
     )
